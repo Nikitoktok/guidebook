f615e5c6fcd9e5589f218d7d5416989327db1db6
Хатунцев Никита <n.a.hatuntse@efko.ru>
2023-04-30 19:07:04 +0300

    Настроена сущность комментариев в админ панели

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -3,7 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+use Symfony\Component\Translation\TranslatableMessage;
 
 class CommentCrudController extends AbstractCrudController
 {
@@ -12,14 +21,40 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Комментарий конференции')
+            ->setEntityLabelInPlural('Комментарий конференции')
+            ->setSearchFields(['author', 'text', 'email'])
+            ->setDefaultSort(['createdAt' => 'DESC']);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(EntityFilter::new('conference'));
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield AssociationField::new('conference', new TranslatableMessage('Конференция'));
+        yield TextField::new('author', new TranslatableMessage('Автор'));
+        yield EmailField::new('email');
+        yield TextareaField::new('text', new TranslatableMessage('Комментарий'))
+            ->hideOnIndex();
+        yield TextField::new('photoFilename', new TranslatableMessage('Путь к фото'))
+            ->onlyOnIndex();
+
+        $createdAt = DateTimeField::new('createdAt', new TranslatableMessage('Дата создания'))->setFormTypeOptions([
+            'years' => range(date('Y'), date('Y') + 5),
+            'widget' => 'single_text',
+        ]);
+
+        if (Crud::PAGE_EDIT === $pageName) {
+            yield $createdAt->setFormTypeOption('disabled', true);
+        } else {
+            yield $createdAt;
+        }
     }
-    */
 }
