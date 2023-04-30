8b6c613322f493551143584b9adeab4b6f9b1b45
Хатунцев Никита <n.a.hatuntse@efko.ru>
2023-04-30 19:20:42 +0300

    Настроена админ страница конференций

--- src/Controller/Admin/ConferenceCrudController.php
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Translation\TranslatableMessage;
 
 class ConferenceCrudController extends AbstractCrudController
 {
@@ -12,14 +18,19 @@ class ConferenceCrudController extends AbstractCrudController
         return Conference::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Конференция')
+            ->setEntityLabelInPlural('Список конференций')
+            ->setSearchFields(['city', 'year']);
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')->hideOnForm();
+        yield TextField::new('city', new TranslatableMessage('Город'));
+        yield IntegerField::new('year', new TranslatableMessage('Год'));
+        yield BooleanField::new('isInternational', new TranslatableMessage('Международная'));
     }
-    */
 }
